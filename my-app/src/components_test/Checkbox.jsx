61aacff6adbb380cdb04f85a82c4089c303c3573
ArtySh0ck <artyshock2015@gmail.com>
2019-02-11 16:55:45 +0200

    Update Checkbox.jsx

--- my-app/src/components_test/Checkbox.jsx
+++ my-app/src/components_test/Checkbox.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 
 class Checkbox extends Component {
+    
+  
+    checkboxChange = (event) => {
+        this.props.updateData(event.target.checked)
+    }
+
     render() {
         return(
-            <input type="checkbox" checked={this.props.checked} ref="checkbox" onChange={() => {
-                this.props.updateData(this.refs.checkbox.checked)
-            }}/>
+            <input type="checkbox" checked={this.props.checked} onChange={this.checkboxChange}/>
         )
+        
     }
+
 }
 
 export default Checkbox
